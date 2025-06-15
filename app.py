0210a86344e2454b1903a9a7384838249d5f1eea
kamlakineh <152890297+kamlakineh@users.noreply.github.com>
2025-06-15 06:24:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,7 @@ def get_video(filename):
     return send_file(path, mimetype="video/mp4")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))  # Use Render's assigned port
+    app.run(host="0.0.0.0", port=port)
+
